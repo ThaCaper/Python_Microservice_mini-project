1228e98c4ee77fcc4383dd0038fb39950622b018
andr7913 <42999988+andr7913@users.noreply.github.com>
2021-05-18 14:58:27 +0200

    orderdb init og orderrepository

--- venv/OrderAPI/DATA/OrderRepository.py
+++ venv/OrderAPI/DATA/OrderRepository.py
@@ -1,6 +1,35 @@
+import sqlite3
+
+
 class OrderRepository:
     # denne skal snakke med sqllite for order
+
     def GetALLOrders(self):
-        return True
+        conn = sqlite3.connect('ORDERSDB.db')
+        cursor = conn.execute("SELECT OrderId, Date, ProductID, Quantity from ORDERS")
+        for row in cursor:
+            print("OrderId = ", row[0])
+            print("Date = ", row[1])
+            print("ProductID = ", row[2])
+            print("Quantity = ", row[3])
+            print("\n")
+        print("Operation done successfully");
+        conn.close()
+
     def getOrdesById(self, id):
-        return True
+        conn = sqlite3.connect('ORDERSDB.db')
+        cursor = conn.execute("SELECT OrderId, Date, ProductID, Quantity from ORDERS WHERE OrderId ="+str(id))
+        for row in cursor:
+            print("OrderId = ", row[0])
+            print("Date = ", row[1])
+            print("ProductID = ", row[2])
+            print("Quantity = ", row[3])
+            print("\n")
+        print("Operation done successfully");
+        conn.close()
+
+
+orr = OrderRepository()
+
+# orr.GetALLOrders()
+orr.getOrdesById(1)
--- venv/OrderAPI/DATA/databaseinit.py
+++ venv/OrderAPI/DATA/databaseinit.py
@@ -1,13 +1,24 @@
+# !/usr/bin/python
+
 import sqlite3
 
-conn = sqlite3.connect('order.db')
 
-conn.execute('''CREATE TABLE Order
-         (ID INT PRIMARY KEY     NOT NULL,
-         Date           Date    NOT NULL,
-         Productid         INT     NOT NULL,
-         Quantity        INT
+conn = sqlite3.connect('ORDERSDB.db')
+print("Opened database successfully");
+
+conn.execute('''CREATE TABLE ORDERS
+         (OrderId INT PRIMARY KEY     NOT NULL,
+         Date           DATE    NOT NULL,
+         ProductID            INT     NOT NULL,
+         Quantity        int
          );''')
+print("Table created successfully");
 
-print("Opened database successfully")
+conn.execute("INSERT INTO ORDERS (OrderId,Date,ProductID,Quantity) \
+      VALUES (1, 2008-11-11, 1, 2 )");
 
+conn.execute("INSERT INTO ORDERS (OrderId,Date,ProductID,Quantity) \
+      VALUES (2, 2008-9-8, 2, 5 )");
+
+conn.commit()
+conn.close()
