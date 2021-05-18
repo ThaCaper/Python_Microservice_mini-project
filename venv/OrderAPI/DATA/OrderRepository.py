7f62a81967a11d8a95eadaf48cdbdc9c0c128a23
andr7913 <42999988+andr7913@users.noreply.github.com>
2021-05-18 16:09:15 +0200

    Update OrderRepository.py

--- venv/OrderAPI/DATA/OrderRepository.py
+++ venv/OrderAPI/DATA/OrderRepository.py
@@ -5,20 +5,22 @@ class OrderRepository:
     # denne skal snakke med sqllite for order
 
     def GetALLOrders(self):
+        OrderList = []
         conn = sqlite3.connect('ORDERSDB.db')
         cursor = conn.execute("SELECT OrderId, Date, ProductID, Quantity from ORDERS")
         for row in cursor:
-            print("OrderId = ", row[0])
-            print("Date = ", row[1])
-            print("ProductID = ", row[2])
-            print("Quantity = ", row[3])
-            print("\n")
-        print("Operation done successfully");
+            OrderId = row[0]
+            Date = row[1]
+            ProductID = row[2]
+            Quantity = row[3]
+            OrderList.append({'orderid': OrderId, 'Date': Date, 'productid': ProductID, 'Quantity': Quantity})
+
         conn.close()
+        return OrderList
 
     def getOrdesById(self, id):
         conn = sqlite3.connect('ORDERSDB.db')
-        cursor = conn.execute("SELECT OrderId, Date, ProductID, Quantity from ORDERS WHERE OrderId ="+str(id))
+        cursor = conn.execute("SELECT OrderId,Date,ProductID,Quantity from ORDERS WHERE OrderId =" + str(id))
         for row in cursor:
             print("OrderId = ", row[0])
             print("Date = ", row[1])
@@ -31,5 +33,7 @@ class OrderRepository:
 
 orr = OrderRepository()
 
-# orr.GetALLOrders()
-orr.getOrdesById(1)
+print(orr.GetALLOrders())
+
+
+# orr.getOrdesById(1)
