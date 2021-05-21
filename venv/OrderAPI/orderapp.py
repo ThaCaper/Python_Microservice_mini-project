818527d21baacc6b32ae6934b2d57cfa1cbb5c1b
andr7913 <42999988+andr7913@users.noreply.github.com>
2021-05-21 10:47:01 +0200

    Update orderapp.py

--- venv/OrderAPI/orderapp.py
+++ venv/OrderAPI/orderapp.py
@@ -44,8 +44,8 @@ def order():
 
     if request.method == 'POST':
         new_date = datetime.datetime.now()
-        new_product_id = request.form['productId']
-        new_quantity = request.form['quantity']
+        new_product_id = request.json["productId"]
+        new_quantity = request.json["quantity"]
         sql_insert_query = """INSERT INTO Orders (date, productId, quantity)
                               VALUES (?, ?, ?)"""
         cur = cursor.execute(sql_insert_query, (new_date, new_product_id, new_quantity))
@@ -53,7 +53,7 @@ def order():
         return f"orders with the id: {cur.lastrowid} created successfully", 201
 
 
-@orderapp.route('/order/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+@orderapp.route('/orders/<int:id>', methods=['GET', 'PUT', 'DELETE'])
 def single_order(id):
     conn = db_connection()
     cursor = conn.cursor()
@@ -75,8 +75,8 @@ def single_order(id):
                                     quantity=?
                                 WHERE id=?"""
         date = datetime.datetime.now()
-        product_id = int(request.form['productId'])
-        quantity = int(request.form['quantity'])
+        product_id = int(request.json["productId"])
+        quantity = int(request.json["quantity"])
 
         updated_order = {
             'id': id,
